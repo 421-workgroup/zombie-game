98b5da3b7aa819e6bbc2b00148fbd02bd555c56f
嵩超 王 <songchaow@outlook.com>
2018-08-07 21:16:08 +0800

    add class Attack.

---
+++ Classes/Attack.cpp
@@ -0,0 +1,7 @@
+#include "Attack.h"
+
+void ZombieBiteAttack::attackRole(RoleBase* role)
+{
+	// TODO: freeze control of that role, drop blood, and make a displacement
+	// on that role's position.
+}
